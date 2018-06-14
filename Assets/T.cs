46bb7f326c7a1b9379890bb0b00efd7a08c20578
zqbb <530055798@qq.com>
2018-06-14 14:15:33 +0800

    VudoriaTestDemo

---
+++ Assets/T.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class T : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.DOLocalJump(new Vector3(0, 0.05f, 0), 2, 1, 1f).SetLoops(-1,LoopType.Restart) ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+	}
+}
